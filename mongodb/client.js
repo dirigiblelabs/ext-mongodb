0bd6683d3215bd67721ae24abc65ac2ff1a79e60
delchev <delchevn@gmail.com>
2020-12-04 00:02:56 +0200

    cover basic mathods

--- mongodb/client.js
+++ mongodb/client.js
@@ -12,7 +12,6 @@
 /** Client API for MongoDB */
 
 
-
 exports.getClient = function() {
 	var client = new Client();
 	var native = org.eclipse.dirigible.api.mongodb.MongoDBFacade.getClient();
@@ -64,11 +63,166 @@ function DBCollection() {
 		this.native.insert(dbObject.native);
 	};
 
-	this.find = function(dbObject) {
+	this.find = function(query, projection) {
 		var dbCursor = new DBCursor();
-		var native = this.native.find(dbObject.native);
+		var native = null;
+		if (query) {
+			if (projection) {
+				native = this.native.find(query.native, projection.native);
+			} else {
+				native = this.native.find(query.native);
+			}
+		} else {
+			native = this.native.find();
+		}
 		dbCursor.native = native;
 		return dbCursor;
+	};
+
+	this.findOne = function(query, projection, sort) {
+		var dbObject = new DBObject();
+		var native = null;
+		if (query) {
+			if (projection) {
+				if (sort) {
+					native = this.native.findOne(query.native, projection.native, sort.native);
+				} else {
+					native = this.native.findOne(query.native, projection.native);
+				}
+			} else {
+				native = this.native.findOne(query.native);
+			}
+		} else {
+			native = this.native.findOne();
+		}
+		dbObject.native = native;
+		return dbObject;
+	};
+
+	this.findOneById = function(id, projection) {
+		var dbObject = new DBObject();
+		var native = null;
+		if (id) {
+			if (projection) {
+				native = this.native.findOne(id, projection.native);
+			} else {
+				native = this.native.findOne(id);
+			}
+		} else {
+			throw new Error("The id must be provided");
+		}
+		dbObject.native = native;
+		return dbObject;
+	};
+
+	this.count = function(query) {
+		if (query) {
+			return this.native.count(query.native);
+		}
+		return this.native.count();
+	};
+
+	this.getCount = function(query) {
+		if (query) {
+			return this.native.getCount(query.native);
+		}
+		return this.native.getCount();
+	};
+
+	this.createIndex = function(keys, options) {
+		if (keys) {
+			if (options) {
+				this.native.createIndex(keys.native, options.native);	
+			} else {
+				this.native.createIndex(keys.native);
+			}
+		} else {
+			throw new Error("At least Keys parameter must be provided");
+		}
+	};
+
+	this.createIndexForField = function(name) {
+		if (name) {
+			this.native.createIndex(name);
+		} else {
+			throw new Error("The filed name must be provided");
+		}
+	};
+
+	this.distinct = function(name, query) {
+		if (name) {
+			if (query) {
+				this.native.distinct(keys.native, query.native);	
+			} else {
+				this.native.distinct(name);
+			}
+		} else {
+			throw new Error("At least the filed name parameter must be provided");
+		}
+	};
+
+	this.dropIndex = function(index) {
+		if (index) {
+			this.native.dropIndex(index);
+		} else {
+			throw new Error("The index parameter must be provided");
+		}
+	};
+
+	this.dropIndexByName = function(name) {
+		if (name) {
+			this.native.dropIndex(name);
+		} else {
+			throw new Error("The index name must be provided");
+		}
+	};
+
+	this.dropIndexes = function() {
+		this.native.dropIndexes();
+	};
+
+	this.remove = function(query) {
+		this.native.remove(query.native);
+	};
+
+	this.rename = function(newName) {
+		this.native.rename(newName);
+	};
+
+	this.save = function(dbObject) {
+		this.native.save(dbObject.native);
+	};
+
+	this.update = function(query, update, upsert, multi) {
+		if (query) {
+			if (update) {
+				if (upsert) {
+					if (multi) {
+						this.native.update(query.native, update.native, upsert, multi);
+					} else {
+						this.native.update(query.native, update.native, upsert);
+					}
+				} else {
+					this.native.update(query.native, update.native);
+				}
+			} else {
+				throw new Error("The query parameter must be provided");
+			}
+		} else {
+			throw new Error("The query parameter must be provided");
+		}
+	};
+
+	this.updateMulti = function(query, update) {
+		if (query) {
+			if (update) {
+				this.native.update(query.native, update.native);
+			} else {
+				throw new Error("The query parameter must be provided");
+			}
+		} else {
+			throw new Error("The query parameter must be provided");
+		}
 	};
 
 }
@@ -85,6 +239,118 @@ function DBCursor() {
 		return dbObject;
 	};
 
+	this.batchSize = function(numberOfElements) {
+		if (!numberOfElements) {
+			throw new Error("The numberOfElements parameter must be provided");
+		}
+		this.native.batchSize(numberOfElements);
+		return this;
+	};
+
+	this.getBatchSize = function() {
+		return this.native.getBatchSize();
+	};
+
+	this.getCollection = function() {
+		var dbCollection = new DBCollection();
+		var native = this.native.getCollection();
+		dbCollection.native = native;
+		return dbCollection;
+	};
+
+	this.getCursorId = function() {
+		return this.native.getCursorId();
+	};
+
+	this.getKeysWanted = function() {
+		var dbObject = new DBObject();
+		var native = this.native.getKeysWanted();
+		dbObject.native = native;
+		return dbObject;
+	};
+
+	this.getLimit = function() {
+		return this.native.getLimit();
+	};
+
+	this.close = function() {
+		this.native.close();
+	};
+
+	this.hasNext = function() {
+		return this.native.hasNext();
+	};
+
+	this.next = function() {
+		var dbObject = new DBObject();
+		var native = this.native.next();
+		dbObject.native = native;
+		return dbObject;
+	};
+
+	this.getQuery = function() {
+		var dbObject = new DBObject();
+		var native = this.native.getQuery();
+		dbObject.native = native;
+		return dbObject;
+	};
+
+	this.length = function() {
+		return this.native.length();
+	};
+
+	this.sort = function(orderBy) {
+		if (!orderBy) {
+			throw new Error("The orderBy parameter must be provided");
+		}
+		this.native.sort(orderBy);
+		return this;
+	};
+
+	this.limit = function(limit) {
+		if (!limit) {
+			throw new Error("The limit parameter must be provided");
+		}
+		this.native.limit(limit);
+		return this;
+	};
+
+	this.min = function(min) {
+		if (!min) {
+			throw new Error("The min parameter must be provided");
+		}
+		this.native.min(min);
+		return this;
+	};
+
+	this.max = function(max) {
+		if (!max) {
+			throw new Error("The max parameter must be provided");
+		}
+		this.native.max(max);
+		return this;
+	};
+
+	this.maxTime = function(maxTime) {
+		if (!maxTime) {
+			throw new Error("The maxTime parameter must be provided");
+		}
+		this.native.maxTime(maxTime, java.util.concurrent.TimeUnit.MILLISECONDS);
+		return this;
+	};
+
+	this.size = function() {
+		return this.native.size();
+	};
+
+	this.skip = function(numberOfElements) {
+		if (!numberOfElements) {
+			throw new Error("The numberOfElements parameter must be provided");
+		}
+		this.native.skip(numberOfElements);
+		return this;
+	};
+
 }
 
 /**
@@ -97,5 +363,39 @@ function DBObject() {
 		return this;
 	};
 
+	this.toJson = function() {
+		return this.native.toJson();
+	}
+
+	this.markAsPartialObject = function() {
+		this.native.markAsPartialObject();
+	}
+
+	this.isPartialObject = function() {
+		return this.native.isPartialObject();
+	}
+
+	this.containsField = function() {
+		return this.native.containsField();
+	}
+
+	this.get = function() {
+		return this.native.get();
+	}
+
+	this.put = function(key, value) {
+		if (!key) {
+			throw new Error("The key parameter must be provided");
+		}
+		if (!value) {
+			throw new Error("The value parameter must be provided");
+		}
+		return this.native.put(key, value);
+	}
+
+	this.removeField = function(key) {
+		return this.native.removeField(key);
+	}
+	
 }
 
